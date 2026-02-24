73fb299fe38a6bac7e08f866772ef3e2caae0c9e
root <root@abk-xps.localdomain>
2026-02-24 13:24:34 -0500

    more detail on tasks

--- trigger.py
+++ trigger.py
@@ -2,7 +2,15 @@ from tasks import run_nmap, run_nuclei
 
 repo = "https://github.com/example/repo"
 
-run_nmap.delay(repo)
-run_nuclei.delay(repo)
+nmap_result = run_nmap.delay(repo)
+nuclei_result = run_nuclei.delay(repo)
 
-print("Tasks submitted")
+print("NMAP scan:")
+print(nmap_result.state)
+print(nmap_result.get())
+print(nmap_result.state)
+
+print("Nuclei scan:")
+print(nuclei_result.state)
+print(nuclei_result.get())
+print(nuclei_result.state)
